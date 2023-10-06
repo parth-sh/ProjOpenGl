282158241e048c39074a55d535a726ec085ed9d6
Parth Sharma <z@Parths-MacBook-Air.local>
2023-10-07 03:28:26 +0530

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
-# Cmake
-rm -rf build/ &&
-mkdir -p build &&
-cmake -S . -B build/ &&
-# Build glfw library, and proj
-make -C build/glfw-3.3.8 && 
-make -C build/glfw-3.3.8/src && 
-make -C build/ &&
+rm -rf build/
+mkdir -p build
+sleep 5
+cmake -S . -B build/
+cmake --build build/
 build/hello
